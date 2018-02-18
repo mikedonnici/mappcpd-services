d242bb02cb1b7d70b9fe80e4afaae68312d0e127
Mike Donnici <michael@mesa.net.au>
2018-02-19 09:58:51 +1100

    remove package aliases

--- cmd/webd/rest/router/handlers/resources.go
+++ cmd/webd/rest/router/handlers/resources.go
@@ -9,35 +9,35 @@ import (
 
 	"github.com/gorilla/mux"
 
-	_json "github.com/mappcpd/web-services/cmd/webd/rest/router/handlers/responder"
-	mw_ "github.com/mappcpd/web-services/cmd/webd/rest/router/middleware"
-	ds_ "github.com/mappcpd/web-services/internal/platform/datastore"
-	r_ "github.com/mappcpd/web-services/internal/resources"
+	"github.com/mappcpd/web-services/cmd/webd/rest/router/handlers/responder"
+	"github.com/mappcpd/web-services/cmd/webd/rest/router/middleware"
+	"github.com/mappcpd/web-services/internal/platform/datastore"
+	"github.com/mappcpd/web-services/internal/resources"
 )
 
 // ResourcesID fetches a single resource from the MySQLConnection db
 func ResourcesID(w http.ResponseWriter, req *http.Request) {
 
-	p := _json.New(mw_.UserAuthToken.Token)
+	p := responder.New(middleware.UserAuthToken.Token)
 	// Request - convert id from string to int type
 	v := mux.Vars(req)
 	id, err := strconv.Atoi(v["id"])
 	if err != nil {
-		p.Message = _json.Message{http.StatusBadRequest, "failed", err.Error()}
+		p.Message = responder.Message{http.StatusBadRequest, "failed", err.Error()}
 	}
 
-	r, err := r_.ResourceByID(id)
+	r, err := resources.ResourceByID(id)
 	// Response
 	switch {
 	case err == sql.ErrNoRows:
-		p.Message = _json.Message{http.StatusNotFound, "failed", err.Error()}
+		p.Message = responder.Message{http.StatusNotFound, "failed", err.Error()}
 	case err != nil:
-		p.Message = _json.Message{http.StatusInternalServerError, "failed", err.Error()}
+		p.Message = responder.Message{http.StatusInternalServerError, "failed", err.Error()}
 	default:
-		p.Message = _json.Message{http.StatusOK, "success", "Data retrieved from " + ds_.MySQL.Source}
+		p.Message = responder.Message{http.StatusOK, "success", "Data retrieved from " + datastore.MySQL.Source}
 		p.Data = r
 		// Sync from MySQLConnection -> MongoDB
-		r_.SyncResource(r)
+		resources.SyncResource(r)
 	}
 
 	p.Send(w)
@@ -47,29 +47,29 @@ func ResourcesID(w http.ResponseWriter, req *http.Request) {
 func ResourcesCollection(w http.ResponseWriter, r *http.Request) {
 
 	// Response
-	p := _json.New(mw_.UserAuthToken.Token)
+	p := responder.New(middleware.UserAuthToken.Token)
 
 	// Pull the JSON body out of the request
 	decoder := json.NewDecoder(r.Body)
-	var q ds_.MongoQuery
+	var q datastore.MongoQuery
 	err := decoder.Decode(&q)
 	if err != nil {
-		p.Message = _json.Message{http.StatusBadRequest, "failure", errMessageDecodeJSON}
+		p.Message = responder.Message{http.StatusBadRequest, "failure", errMessageDecodeJSON}
 		p.Send(w)
 		return
 	}
 
 	var res []interface{}
-	res, err = r_.QueryResourcesCollection(q)
+	res, err = resources.QueryResourcesCollection(q)
 	if err != nil {
-		p.Message = _json.Message{http.StatusInternalServerError, "failed", err.Error()}
+		p.Message = responder.Message{http.StatusInternalServerError, "failed", err.Error()}
 		p.Send(w)
 		return
 	}
 
-	p.Message = _json.Message{http.StatusOK, "success", "Data retrieved from " + ds_.MongoDB.Source}
+	p.Message = responder.Message{http.StatusOK, "success", "Data retrieved from " + datastore.MongoDB.Source}
 	c := len(res)
-	p.Meta = _json.MongoMeta{c, q}
+	p.Meta = responder.MongoMeta{c, q}
 	p.Data = res
 	p.Send(w)
 }
@@ -78,31 +78,31 @@ func ResourcesCollection(w http.ResponseWriter, r *http.Request) {
 func ResourcesLatest(w http.ResponseWriter, r *http.Request) {
 
 	// Response
-	p := _json.Payload{}
+	p := responder.Payload{}
 
 	// Request - convert id from string to int type
 	v := mux.Vars(r)
 	n, err := strconv.Atoi(v["n"])
 	if err != nil {
-		p.Message = _json.Message{http.StatusBadRequest, "failed", err.Error()}
+		p.Message = responder.Message{http.StatusBadRequest, "failed", err.Error()}
 	}
 
 	// Grab the latest...
-	var q ds_.MongoQuery
+	var q datastore.MongoQuery
 	q.Limit = n
 	q.Sort = "-createdAt"
 
 	var res []interface{}
-	res, err = r_.QueryResourcesCollection(q)
+	res, err = resources.QueryResourcesCollection(q)
 	if err != nil {
-		p.Message = _json.Message{http.StatusInternalServerError, "failed", err.Error()}
+		p.Message = responder.Message{http.StatusInternalServerError, "failed", err.Error()}
 		p.Send(w)
 		return
 	}
 
-	p.Message = _json.Message{http.StatusOK, "success", "Data retrieved from " + ds_.MongoDB.Source}
+	p.Message = responder.Message{http.StatusOK, "success", "Data retrieved from " + datastore.MongoDB.Source}
 	c := len(res)
-	p.Meta = _json.MongoMeta{c, q}
+	p.Meta = responder.MongoMeta{c, q}
 	p.Data = res
 	p.Send(w)
 }
