a991bd27e5223c0f9174c3dc40fc4964f808a477
Mike Donnici <michael@mesa.net.au>
2018-02-19 10:13:36 +1100

    Update services script after resolution of #20

--- run_services.sh
+++ run_services.sh
@@ -3,14 +3,11 @@
 echo "Running pubmedr #####################################################################"
 pubmedr
 
-echo "Running fixr -t pubmedData #########################################################"
-fixr -b 1 -t "pubmedData"
+echo "Running fixr #########################################################"
+fixr -b 1 -t "pubmedData,fixResources"
 
 echo "Running mongr #####################################################################"
 mongr -b 1 -c all
-
-echo "Running fixr -t fixResources #####################################################"
-fixr -b 1 -t "fixResources"
 
 echo "Running algr #####################################################################"
 algr -b 1
